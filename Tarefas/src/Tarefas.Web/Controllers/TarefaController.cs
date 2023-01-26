dd983622ca1d8ab2f009004db42d5dfb39b2aaa6
Patrick <Patrickjhonsom@gmail.com>
2023-01-25 21:17:12 -0300

    Adicionando libs Para conexão com banco de Dados SQLite

---
+++ Tarefas/src/Tarefas.Web/Controllers/TarefaController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Tarefas.Web.Models;
+
+namespace Tarefas.Web.Controllers;
+
+public class TarefasController : Controller
+{ 
+      public List<TarefaViewModel> listaDeTarefas{get;set;}
+     public TarefasController()
+   {
+           listaDeTarefas = new List<TarefaViewModel>(){
+           new TarefaViewModel(){id=1,Titulo = "Escovar os dentes",Descrisao="Teste"}, 
+           new TarefaViewModel(){id=2,Titulo = "ArrumarCama",Descrisao="Teste"},
+           new TarefaViewModel(){id=3,Titulo = "Escovar os dentes",Descrisao="Teste"}
+        };
+   }
+  
+    public IActionResult Create()
+    {
+        return View();
+    }
+        public IActionResult Index()
+    {
+ 
+        return View(listaDeTarefas);
+    }
+       public IActionResult Detalis(int id) 
+        {
+          var tarefa  = listaDeTarefas.Find(tarefa=>tarefa.id==id);
+          return View(tarefa);//Contruir View De detalhe
+          //vert automapper c# aspnetcore
+        }
+
+
+}
